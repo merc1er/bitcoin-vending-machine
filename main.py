d9b4be15df929cb431868022e5dfa91e6bf5e1b6
Corentin MERCIER <corentin.mercier@epita.fr>
2018-06-10 23:56:20 +0800

    Design improved + order page created

--- main.py
+++ main.py
@@ -11,4 +11,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# order
+@app.route('/<item>')
+def order(item=None):
+    return render_template('order.html', item=item)
+
 app.run(debug=True, port=666, host='127.0.0.1')
