27f67a2108583f39a71a8351cc8308af1128cb29
Corentin MERCIER <corentin.mercier@epita.fr>
2018-07-07 16:44:18 +0800

    Price rounded more

--- lib/price.py
+++ lib/price.py
@@ -6,5 +6,5 @@ def get():
     req = requests.get('https://api.coinbase.com/v2/prices/bch-usd/buy')
     json = req.json()
     bch_price = float(json['data']['amount'])
-    price = round(1 / bch_price, 6)
+    price = 1 / bch_price
     return price
