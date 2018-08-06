605f12e9885e8141d0c8fc4b104bfe01a757f7bd
Corentin MERCIER <corentin.mercier@epita.fr>
2018-08-06 15:42:13 +0800

    Init.d script

--- vending.sh
+++ vending.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo python3 ~/Desktop/main.py
+
+# to be placed in /etc/init.d/
+# to run once: sudo update-rc.d vending.sh defaults
+sudo python3 /home/pi/Desktop/bitcoin-vending-machine/main.py
