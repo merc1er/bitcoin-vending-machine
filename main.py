0583de036e34a944fe575152e0380a98a5ddcd1b
Corentin MERCIER <corentin.mercier@epita.fr>
2018-08-07 11:00:34 +0800

    Price fetched from another process

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def index():
 def order(item=None):
     # fetch the correct price here
     import lib.price as price
-    can_price = price.get()
+    can_price = getprice()
     url = your_address + '?amount='
     url += str(can_price)
     img = qrcode.make(url)
@@ -27,6 +27,10 @@ def order(item=None):
     return render_template('order.html', item=item, price=round(can_price, 4),
                                             exact_price=can_price)
 
+def getprice():
+    with open('price', 'r') as f:
+        price = f.readline()
+    return float(price)
 
 ####################
 # checking the payment
