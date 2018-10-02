b0517d153bed585b7c119569096a593b331a13cb
merc1er <corentin@mercier.link>
2018-10-02 12:57:43 +0800

    Sleep time removed

--- lib/dispense.py
+++ lib/dispense.py
@@ -1,10 +1,6 @@
-from time import sleep
-
 def dispense(item):
     """ Dispenses the correct item
     """
     from gpiozero import LED
     led = LED(21) # row 21 used (anode)
     led.on()
-    sleep(5)
-    led.off()
